502a8d2c0c84dba5a4b538aa0c75e01c11aeb51e
Simonjoergensen <simon_hasselholt@yahoo.dk>
2022-05-31 16:35:52 +0200

    added new property

--- recipes_and_meal_plan_api/Models/Users.cs
+++ recipes_and_meal_plan_api/Models/Users.cs
@@ -8,5 +8,6 @@ namespace recipes_and_meal_plan_api.Models {
         public int Id { get; set; } 
         public string Username { get; set; }
         public string Password { get; set; }
+        public bool IsAdmin { get; set; }
     }
 }
